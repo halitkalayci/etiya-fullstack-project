e2a2c183ec4b20607da24ecebf34764527e489c4
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-09-22 14:23:59 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'tr',
+      useDefaultLang: true,
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (httpClient: HttpClient) => {
+          return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
+        },
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
