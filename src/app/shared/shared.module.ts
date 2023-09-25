3cd429587b6730bbfeeed0292dcc2e32e433b2f7
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-09-25 10:19:20 +0300

    login page translations

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { SharedRoutingModule } from './shared-routing.module';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { LoginComponent } from './pages/login/login.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [HomepageComponent, MainLayoutComponent, LoginComponent],
-  imports: [CommonModule, SharedRoutingModule],
+  imports: [CommonModule, SharedRoutingModule, TranslateModule],
   exports: [MainLayoutComponent], // dış modüllerin kullanabileceği, dışarıya export edilen modüller
 })
 export class SharedModule {}
