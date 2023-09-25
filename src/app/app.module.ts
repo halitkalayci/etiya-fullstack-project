a63cd414ee78d7a47dee289a72c79d961b89f573
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-09-25 09:33:22 +0300

    main layout & homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [AppComponent],
+  // Import ettiğim modüllerden sadece o modülün export ettiği alanları kullanabilirim.
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
-
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    SharedRoutingModule
-  ]
+  declarations: [HomepageComponent, MainLayoutComponent, LoginComponent],
+  imports: [CommonModule, SharedRoutingModule],
+  exports: [MainLayoutComponent], // dış modüllerin kullanabileceği, dışarıya export edilen modüller
 })
-export class SharedModule { }
+export class SharedModule {}
