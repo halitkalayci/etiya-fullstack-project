7f12e5df7302ff2a0ceb7c356449203228fdd2a5
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-09-25 10:26:04 +0300

    layout toggle by url

--- src/app/shared/components/main-layout/main-layout.component.ts
+++ src/app/shared/components/main-layout/main-layout.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.css']
+  styleUrls: ['./main-layout.component.css'],
 })
-export class MainLayoutComponent {
+export class MainLayoutComponent implements OnInit {
+  layoutDisabledUrls: string[] = ['/login', '/register'];
+  showLayout: boolean = true;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((value) => {
+      if (value instanceof NavigationStart) {
+        if (this.layoutDisabledUrls.includes(value.url)) {
+          this.showLayout = false;
+        } else {
+          this.showLayout = true;
+        }
+      }
+    });
+  }
 }
