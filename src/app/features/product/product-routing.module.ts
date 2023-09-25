88dd021e9a4f5d9e68de272080dd957a901e8513
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-09-25 14:19:44 +0300

    product pages

--- src/app/features/product/product-routing.module.ts
+++ src/app/features/product/product-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ProductRoutingModule { }
+export class ProductRoutingModule {}
