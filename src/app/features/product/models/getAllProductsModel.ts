65898fe2ccf1dabb4e78f631f8efedbfe6c961cd
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-09-26 09:40:39 +0300

    environment & model & service

---
+++ src/app/features/product/models/getAllProductsModel.ts
@@ -0,0 +1,13 @@
+// CTRL + ALT + V
+export interface Category {
+  categoryId: number;
+  categoryName: string;
+  description: string;
+}
+
+export interface GetAllProductsModel {
+  id: number;
+  name: string;
+  unitPrice: number;
+  category: Category;
+}
