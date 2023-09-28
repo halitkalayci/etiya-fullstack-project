c388abb11c047b46d365ea08a7293c36bb5dff57
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-09-28 13:37:46 +0300

    socketio

--- server/main.js
+++ server/main.js
@@ -6,15 +6,34 @@ const app = express();
 
 const server = http.createServer(app);
 
-const io = new socketio.Server(server);
+const io = new socketio.Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
 
 app.get('/', (req, res) => {
   res.send("Hello World");
 })
-
+// io => server
+// socket => client
 io.on('connection', (socket) => {
-  // handshake
-  console.log("a user connected")
+  socket.on('disconnect', () => {
+    console.log("user disconnected")
+  })
+  socket.on('sendMessage', (message, sender) => {
+    // io.emit => servera bağlı tüm clientlara (çağıran kişide dahil) sinyal gönderir
+    // socket.emit => sadece çağıran kişiye sinyal gönderir
+    // socket.brodcast.emit => çağıran kişi hariç diğer tüm bağlı clientlara sinyal gönderir..
+
+    // kanallar =>
+    // socket.brodcast.to('kanalIsmi').emit => bir kanala üye olmuş gönderen kişi hariç tüm clientlara sinyal gönderir
+    // io.in('kanalIsmi').emit() => kanaldaki gönderen kişi dahil herkese sinyal gönderir
+
+    // id=>
+    // socket.braodcast.to(id) => spesifik tek bir kişiye (id varsa) mesaj gönderir..
+    socket.broadcast.emit("messageSent", message, sender);
+  })
 })
 
 app.get('/products', (req, res) => {
--- src/app/shared/pages/homepage/homepage.component.ts
+++ src/app/shared/pages/homepage/homepage.component.ts
@@ -6,9 +6,21 @@ import { Socket } from 'ngx-socket-io';
   styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
+  message: string = '';
+  username: string = '';
+  messageHistory: any = [];
   constructor(private socket: Socket) {}
 
   ngOnInit() {
-    console.log(this.socket);
+    // socket => server
+    this.socket.on('messageSent', (message: string, sender: string) => {
+      //
+      this.messageHistory.push({ message, sender });
+    });
+  }
+
+  sendMessage() {
+    this.socket.emit('sendMessage', this.message, this.username);
+    this.messageHistory.push({ message: this.message, sender: 'Siz' });
   }
 }
